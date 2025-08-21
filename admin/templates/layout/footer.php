b656687a5ad4e339db07d30a0338520791fdd8fd
khangdz <khangdz@Mac.lan>
2025-08-21 17:34:19 +0700

    update

--- admin/templates/layout/footer.php
+++ admin/templates/layout/footer.php
@@ -1,7 +1,7 @@
 <!-- Main Footer -->
 <footer class="main-footer text-center text-sm">
-    <p class="mb-1">CÔNG TY TNHH THƯƠNG MẠI VÀ DỊCH VỤ NINA</p>
-    <p class="mb-1">Địa chỉ: Lầu 3, Tòa nhà SaigonTel, Lô 46, CVPM Quang Trung, P. Tân Chánh Hiệp, Q. 12, TP HCM</p>
-    <p class="mb-1">Tel: 028.37154879 - Fax: 028.37154878</p>
-    <p class="mb-0">Email: nina@nina.vn</p>
+    <p class="mb-1">CÔNG TY TNHH THƯƠNG MẠI VÀ DỊCH VỤ PHU AN KHANG</p>
+    <p class="mb-1">Địa chỉ: 385D Tỉnh Lộ 15, Huyện Củ Chi, Xã Phú Hoà Đông, TP HCM</p>
+    <p class="mb-1">Tel: 0978676722</p>
+    <p class="mb-0">Email: phukhangdoduong2@gmail.com</p>
 </footer>
